785aa26954ba2339dbfb21114871b19c2d8a4c6b
Beddaira Hatem <hatembeddaira@gmail.com>
2026-05-20 23:34:55 +0200

    commit dev

--- src/AceRental.Domain/Entities/Client.cs
+++ src/AceRental.Domain/Entities/Client.cs
@@ -5,12 +5,16 @@ namespace AceRental.Domain.Entities
     public class Client : BaseEntity
     {
         public required string ClientNumber { get; set; }
+        public string? RaisonSociale { get; set; }
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
         public required string Email { get; set; }
         public string? PhoneNumber { get; set; }
+        public string? TelNumber { get; set; }
         public string? Address { get; set; }
-        
+        public string? ComplementAdresse { get; set; }
+        public string? City { get; set; }
+        public int PostalCode { get; set; }
         public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
     }
 }
